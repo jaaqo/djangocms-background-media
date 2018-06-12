6f5a714d7aca6a880af2c9d797b78118b40a3d20
Jaakko Salokanta <jaakko.salokanta@cybercom.com>
2018-06-12 15:27:39 +0300

    Edit background video fieldtype

--- djangocms_background_media/models.py
+++ djangocms_background_media/models.py
@@ -8,6 +8,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from cms.models import CMSPlugin
 from filer.fields.image import FilerImageField
+from filer.fields.file import FilerFileField
 
 
 @python_2_unicode_compatible
@@ -28,15 +29,25 @@ class BackgroundMedia(CMSPlugin):
     image_position = models.CharField(_('image position'), max_length=50, default='center center',
         help_text=_('The css position of the image'))
 
-    video_mp4 = models.FileField(_('Video mp4'), blank=True, null=True,
-        upload_to='videos/djangocms_bg_media', help_text=_('Video in the mp4 format'))
-    video_ogv = models.FileField(_('Video ogv'), blank=True, null=True,
-        upload_to='videos/djangocms_bg_media', help_text=_('Video in the ogv format'))
-    video_webm = models.FileField(_('Video webm'), blank=True, null=True,
-        upload_to='videos/djangocms_bg_media', help_text=_('Video in the webm format'))
-    video_poster = FilerImageField(verbose_name=_('Video poster'), blank=True, null=True)
-    vide_json_config = models.TextField(_('Vide.js config'), blank=True, null=True,
-        help_text=_('The optional js object for the vide config. Click <a href="https://github.com/VodkaBears/Vide" target="_blank"> here</a> for more info.'))
+    video_mp4 = FilerFileField(verbose_name=_('Video mp4'),
+                               blank=True,
+                               null=True,
+                               related_name='video_mp4')
+    video_ogv = FilerFileField(verbose_name=_('Video ogv'),
+                               blank=True,
+                               null=True,
+                               related_name='video_ogv')
+    video_webm = FilerFileField(verbose_name=_('Video webm'),
+                                blank=True,
+                                null=True,
+                                related_name='video_webm')
+    video_poster = FilerImageField(verbose_name=_('Video poster'),
+                                   blank=True,
+                                   null=True)
+    vide_json_config = models.TextField(_('Vide.js config'),
+                                        blank=True,
+                                        null=True,
+                                        help_text=_('The optional js object for the vide config. Click <a href="https://github.com/VodkaBears/Vide" target="_blank"> here</a> for more info.'))
 
     def __str__(self):
         return self.name
